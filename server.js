24884a3f23db9d0029310347a2619bdf50aac88b
Patrick Yambrick <patrickyambrick@gmail.com>
2022-04-27 13:11:39 -0400

    Set up server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require("path");
+
+
+const app = express();
+const db_tools = require('./db/connection');
+
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(require('./routes/info'));
+app.use(require('./routes/garden'));
+
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+})
+
+
+app.listen(port, () => {
+  // perform a database connection when server starts
+  db_tools.connectToServer((err) => {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
