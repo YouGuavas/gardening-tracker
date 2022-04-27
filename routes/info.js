2a024fab8fde7f5596d3ceacc8a104c5383e1b3d
Patrick Yambrick <patrickyambrick@gmail.com>
2022-04-27 13:26:56 -0400

    Add api for fetching plant info

--- routes/info.js
+++ routes/info.js
@@ -8,8 +8,15 @@ const db_tools = require('../db/connection');
 const ObjectId = require("mongodb").ObjectId;
 
 
-router.route('/info').get((req, res) => {
-  res.send('hello world')
+router.route('/information/:plant_type').get((req, res) => {
+  let db_connect = db_tools.getDB('plants');
+  db_connect
+    .collection(req.params.plant_type)
+    .find({})
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.json(result);
+    })
 });
 
 module.exports = router;
