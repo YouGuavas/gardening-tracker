32552ab3da389038219d8844bad30228cc427442
Patrick Yambrick <patrickyambrick@gmail.com>
2022-05-09 13:10:18 -0400

    Initialize Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
+import '../styles/Login.scss';
+
+
 export const Login = (props) => {
   return (
-    <div className="main">
-      <form>
-        Log in here:
+    <div className="main form-container">
+
+
+      <form name="login">
+
+        <h4 className="form-title">Log in here:</h4>
+
+        <div className="container-full">
+          <label for="username">Username:</label>
+          <input type="text" name="username" placeholder="Username"/>
+        </div>
+
+        <div className="container-full">
+          <label for="password">Password:</label>
+          <input type="password" name="password" placeholder="Password"/>
+        </div>
+
+        <div className="container-full">
+          <input className="submit-button" type="submit" name="submit"/>
+        </div>
+
       </form>
-      <form>
-        Not a user? Register here:
+
+
+      <form name="register">
+
+        <h4 className="form-title">Not a user? Register here:</h4>
+
+        <div className="container-full">
+          <label for="username">Username:</label>
+          <input type="text" name="username" placeholder="Username"/>
+        </div>
+
+        <div className="container-full">
+          <label for="email">Email:</label>
+          <input type="email" name="email" placeholder="Email"/>
+        </div>
+
+        <div className="container-full">
+          <label for="password">Password:</label>
+          <input type="password" name="password" placeholder="Password"/>
+        </div>
+
+        <div className="container-full">
+          <input className="submit-button" type="submit" name="submit"/>
+        </div>
+
       </form>
+
+
     </div>
   )
 }
