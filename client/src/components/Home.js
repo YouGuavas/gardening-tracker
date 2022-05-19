6ea81efd121648b493eef6829d0fc00128278a04
Patrick Yambrick <patrickyambrick@gmail.com>
2022-05-19 17:21:49 -0400

    Edit Home content

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,20 @@
+import '../styles/Home.scss';
+
+
 export function Home() {
   return (
-    <div className="main grid">
+    <div className="main">
       <h1>Home</h1>
-      <a className="" href="/info">Info</a>
-      <a href="/garden">Garden</a>
+      <section className="section">
+        <h2>Welcome to the garden tracker!</h2>
+        <p>Users may use this tool to keep track of the plants in their garden, 
+          and to learn some basic information about certain species of plant. 
+          Current plant types in the database include:</p>
+        <ul>
+          <li>Peppers</li>
+        </ul>
+        
+      </section>
 
     </div>
   )
