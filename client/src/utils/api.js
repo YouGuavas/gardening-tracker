ca0707a72a3216fc6d42ba3d8a948ad8513fa141
Patrick Yambrick <patrickyambrick@gmail.com>
2022-05-23 08:52:50 -0400

    Heroku uri change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const api_uri = 'http://localhost:5000/';
+const api_uri = 'https://guavas-garden-tracker.herokuapp.com/';
 const axios = require('axios');
 
 const getPlantsByType = (typeOfPlant) => {
